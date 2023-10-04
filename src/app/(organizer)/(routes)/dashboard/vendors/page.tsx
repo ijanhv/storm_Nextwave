7f4537a7ce211feb0f7ebc916dc917123829e8ef
techatikin <nikitakhabya03@gmail.com>
2023-10-05 02:58:32 +0530

    feat: filtering for vendor listing

--- src/app/(organizer)/(routes)/dashboard/vendors/page.tsx
+++ src/app/(organizer)/(routes)/dashboard/vendors/page.tsx
@@ -1,6 +1,8 @@
+'use client'
 import { BuildingStorefrontIcon, VideoCameraIcon, SparklesIcon, MusicalNoteIcon } from "@heroicons/react/24/outline"
 import VendorCard from "@/components/dashboard/vendors/VendorCard"
 import FilterVendors from "@/components/dashboard/organizerVendors/FilterVendors"
+import { useState } from "react"
 
 const vendorsList = [
   {
@@ -42,16 +44,29 @@ const vendorsList = [
 ]
 
 const VendorsPage = () => {
+  const categories = ['All', 'Decoration', 'Music', 'Venue', 'Photography', 'Catering']
+  const [vendorCategory, setVendorCategory] = useState('All')
+
+  const handleFilter = (vendor: any) => {
+    if (vendorCategory === 'All') return true
+
+    return vendor.category.toLowerCase() === vendorCategory.toLowerCase()
+  }
+
   return (
     <div className='m-5'>
       <h2 className='heading'>Vendors</h2>
 
       <div className='border flex flex-col justify-end border-gray-300 px-6 py-2 my-5 rounded-lg'>
 
-        <FilterVendors />
+        <FilterVendors
+          categories={categories}
+          vendorCategory={vendorCategory}
+          setVendorCategory={setVendorCategory}
+        />
 
         <div className='grid grid-cols-3 gap-x-9 gap-y-7 my-7 h-[450px] overflow-x-hidden overflow-y-scroll'>
-          {vendorsList.map((vendor, index) => (
+          {(vendorCategory !== 'all' ? vendorsList.filter(handleFilter) : vendorsList).map((vendor, index) => (
             <VendorCard
               index={index}
               key={index}
--- src/components/dashboard/organizerVendors/FilterVendors.tsx
+++ src/components/dashboard/organizerVendors/FilterVendors.tsx
@@ -1,24 +1,33 @@
 'use client'
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
-import { useState } from "react"
+import { FormEvent, useState } from "react"
 
-const FilterVendors = () => {
-  const [category, setCategory] = useState('All')
-  const categories = ['All', 'Photography', 'Catering', 'Decoration', 'Music', 'Venue', 'Others']
+type Props = {
+  vendorCategory: string
+  setVendorCategory: (category: string) => void
+  categories: string[]
+}
+
+const FilterVendors = ({ vendorCategory, setVendorCategory, categories }: Props) => {
+
+  const handleCategoryChange = (category: FormEvent<HTMLSelectElement>) => {
+    setVendorCategory((category.target as HTMLSelectElement).value)
+  }
 
   return (
-    <Select>
-      <SelectTrigger className="w-[360px]">
-        <SelectValue placeholder="Select Venue" />
-      </SelectTrigger>
-      <SelectContent>
-        {categories.map((category, index) => (
-          <div key={index}>
-            <SelectItem value={category}>{category}</SelectItem>
-          </div>
+    <div className='flex justify-start items-center space-x-10'>
+      <select
+        value={vendorCategory}
+        onChange={(e) => handleCategoryChange(e)}
+        name='role' id='role'
+        className='bg-white p-3 focus:outline-none font-bold text-gray-700 rounded-lg border border-gray-200 w-1/2'
+      >
+        {categories.map((category) => (
+          <option key={category} value={category}>{category}</option>
         ))}
-      </SelectContent>
-    </Select>
+      </select>
+      <p className='p-1 px-2 text-center w-40 rounded-full bg-orange-200'>{vendorCategory}</p>
+    </div>
   )
 }
 
